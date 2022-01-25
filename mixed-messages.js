35c0550003f031b41b4c800f0dd4287c1e9f658b
Erick Jackaman <erickjackaman@gmail.com>
2022-01-25 15:16:10 +0800

    build random message first attempt

--- mixed-messages.js
+++ mixed-messages.js
@@ -4,7 +4,24 @@
 // Message idea: greeting + colour + animal
 // Additional comment test
 
+//Array to store greetings
 var greetings = ["hello", "bonjour", "greetings", "hola", "welcome", "hi", "hey there"];
+//Array to store colours
 var colours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+//Array to store animals
 var animals = ["tiger", "bear", "rat", "pigeon", "peacock", "anaconda", "panda", "firefly"];
 
+//Random number generator between 0 and 7 
+var randomNumber = Math.floor(Math.random() * 8);
+
+var randomGreeting = greetings[randomNumber];
+var randomColour = colours[randomNumber];
+var randomAnimal = animals[randomNumber];
+
+randomGreeting;
+randomColour;
+randomAnimal;
+
+var messageString = randomGreeting + " " + randomColour + " " + randomAnimal;
+
+console.log(messageString);
