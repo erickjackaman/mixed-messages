d16eca144804b57af9fe4b31e89bdf7d3e7219ca
Erick Jackaman <erickjackaman@gmail.com>
2022-01-25 15:25:59 +0800

    Create function for random number

--- mixed-messages.js
+++ mixed-messages.js
@@ -11,17 +11,18 @@ var colours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 //Array to store animals
 var animals = ["tiger", "bear", "pigeon", "peacock", "anaconda", "panda", "firefly"];
 
-//Random number generator between 0 and 6 
-var randomNumber = Math.floor(Math.random() * 7);
+//Create function to generate random number between 0 and 6
+function getRandomInt() {
+    return Math.floor(Math.random() * 7);
+  }
 
-var randomGreeting = greetings[randomNumber];
-var randomColour = colours[randomNumber];
-var randomAnimal = animals[randomNumber];
+//Create variable for each part of the message
+var randomGreeting = greetings[getRandomInt()];
+var randomColour = colours[getRandomInt()];
+var randomAnimal = animals[getRandomInt()];
 
-randomGreeting;
-randomColour;
-randomAnimal;
-
+//Create message string
 var messageString = randomGreeting + " " + randomColour + " " + randomAnimal;
 
+//Print message to console
 console.log(messageString);
